4f34104e59293d33cc5c56b81f5b5efe382051f0
Hamza Hrnjicevic <hamzazee@gmail.com>
2018-05-24 16:11:39 +0200

    deviceready v3

--- js/my-app.js
+++ js/my-app.js
@@ -441,3 +441,18 @@ $$(document).on('page:init', function (e, page) {
 $$(document).on('deviceready', function(){
     console.log("deviceready is ready");
 });
+
+ function calendar_events(){
+ var startDate = new Date(2014,4,31,18,30,0,0,0); // beware: month 0 = january, 11 = december
+  var endDate = new Date(2014,4,31,19,30,0,0,0);
+  var title = "Skype meeting With Lead";
+ //var location = "myleads.html";
+  var notes = "Need to do a skype meeting with the lead.";
+  var success = function(message) { alert("Success: " + JSON.stringify(message)); };
+  var error = function(message) { alert("Error: " + message); };
+  // window.plugins.calendar.listEventsInRange(startDate,endDate,success,error);
+
+//  window.plugins.calendar.createCalendar(calendarName,success,error);
+  window.plugins.calendar.createEvent(title,location,notes,startDate,endDate,success,error);
+ }
+ document.addEventListener("deviceready", calendar_events, false);
